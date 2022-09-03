b36fd15cdde18a8fb5fac12b92925437e1fffbab
ashimbhowmik <ashimbhowmik239@gmail.com>
2022-09-03 19:45:27 +0600

    [card re design]

--- js/news.js
+++ js/news.js
@@ -97,14 +97,14 @@ const displayNews = (allNews) => {
                 <ul tabindex="0"
                     class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                     <li><button>Home</button></li>
-                    <li><button>Breaking News</button></li>
-                    <li><button>Regular news</button></li>
-                    <li><button>International News</button></li>
-                    <li><button>Sports</button></li>
-                    <li><button>Entertainment</button></li>
-                    <li><button>Culture</button></li>
-                    <li><button>Arts</button></li>
-                    <li><button>All News</button></li>
+                    <li><button onclick="loadNewsDetails('${allNews[0].category_id}')">${allNews[0].category_name}</button></li>
+                    <li><button onclick="loadNewsDetails('${allNews[1].category_id}')">${allNews[1].category_name}</button></li>
+                    <li><button onclick="loadNewsDetails('${allNews[2].category_id}')">${allNews[2].category_name}</button></li>
+                    <li><button onclick="loadNewsDetails('${allNews[3].category_id}')">${allNews[3].category_name}</button></li>
+                    <li><button onclick="loadNewsDetails('${allNews[4].category_id}')">${allNews[4].category_name}</button></li>
+                    <li><button onclick="loadNewsDetails('${allNews[5].category_id}')">${allNews[5].category_name}</button></li>
+                    <li><button onclick="loadNewsDetails('${allNews[6].category_id}')">${allNews[6].category_name}</button></li>
+                    <li><button onclick="loadNewsDetails('${allNews[7].category_id}')">${allNews[7].category_name}</button></li>
                 </ul>
             </div>
         </div>
@@ -136,56 +136,64 @@ const loadNewsDetails = async category_id => {
     const url = ` https://openapi.programming-hero.com/api/news/category/${category_id}`;
     const res = await fetch(url);
     const data = await res.json();
-    displayNewsAll(data.data);
+    const sortByView = await data.data.sort((a, b) => b.total_view - a.total_view);
+    displayNewsAll(sortByView);
 }
 
 const displayNewsAll = idNumber => {
 
-    // console.log(idNumber);
+    console.log(idNumber);
+
+    const totalNewsFounded = document.getElementById('news-found');
+    totalNewsFounded.innerText = idNumber.length;
+
     const newsShows = document.getElementById('news-container');
     newsShows.innerHTML = '';
     idNumber.forEach(news => {
         // console.log(news); 
         const newsDiv = document.createElement('div');
         newsDiv.innerHTML = `
-        <div class="card lg:card-side bg-base-200 shadow-xl mb-7">
-            <figure><img class="w-96" src="${news.thumbnail_url}" alt="Album"></figure>
-            <div class="card-body">
-                    <h2 class="card-title">${news.title}</h2>
-                    <p>${news.details} </p>
-                    <div class="card-actions justify-between items-center mt-8 lg:mt-0">
-                        <div class="flex mb-10 lg:mb-0">
-                            <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-                                <div class="w-10 rounded-full">
-                                    <img src="${news.author.img}" />
-                                </div>
-                            </label>
-                            <div class="ml-2 lg:ml-5">
-                                <p>${news.author.name}</p>
-                                <p>${news.author.published_date}</p>
+        <div class="flex py-4 px-4 mb-5 drop-shadow-xl border">
+            <div class="w-3/12 ">
+                <figure><img class="w-full h-96" src="${news.thumbnail_url}" alt="Album"></figure>
+            </div>
+            <div class="w-9/12 ml-4">
+                <h1 class="mb-4">${news.title}</h1>
+                <p class="h-48 mb-4 text-ellipsis overflow-hidden ">${news.details}</p>
+                <p class="mb-6 truncate  ">${news.details}</p>
+                <div class="card-actions justify-between items-center mt-8 lg:mt-0">
+                    <div class="flex mb-10 lg:mb-0">
+                        <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                            <div class="w-10 rounded-full">
+                                <img src="${news.author.img}" />
                             </div>
+                        </label>
+                        <div class="ml-2 lg:ml-5">
+                            <p>${news.author.name}</p>
+                            <p>${news.author.published_date}</p>
                         </div>
-
-                        <div class="flex mb-10 lg:mb-0">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                            </svg>
-
-                            <p class="ml-3"> ${news.total_view}M</p>
-                        </div>
-                        <div class="rating">
-                            <input type="radio" name="rating-1" class="mask mask-star" />
-                            <input type="radio" name="rating-1" class="mask mask-star" checked />
-                            <input type="radio" name="rating-1" class="mask mask-star" />
-                            <input type="radio" name="rating-1" class="mask mask-star" />
-                            <input type="radio" name="rating-1" class="mask mask-star" />
-                        </div>
-                        <label onclick="newsDetail('${news._id}')" for="my-modal-3" class="btn modal-button"><i class="fa-solid fa-arrow-right"></i></label>
                     </div>
+
+                    <div class="flex mb-10 lg:mb-0">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        </svg>
+
+                        <p class="ml-3"> ${news.total_view}M</p>
+                    </div>
+                    <div class="rating">
+                        <input type="radio" name="rating-1" class="mask mask-star" />
+                        <input type="radio" name="rating-1" class="mask mask-star" checked />
+                        <input type="radio" name="rating-1" class="mask mask-star" />
+                        <input type="radio" name="rating-1" class="mask mask-star" />
+                        <input type="radio" name="rating-1" class="mask mask-star" />
+                    </div>
+                    <label onclick="newsDetail('${news._id}')" for="my-modal-3" class="btn modal-button"><i
+                            class="fa-solid fa-arrow-right"></i></label>
+                </div>
             </div>
         </div>
 
@@ -231,6 +239,6 @@ const toggleSpinner = isLoading => {
         loaderSection.classList.add('hidden');
     }
 }
-newsDetail();
-loadNewsDetails();
+// newsDetail();
+// loadNewsDetails();
 loadNews();
